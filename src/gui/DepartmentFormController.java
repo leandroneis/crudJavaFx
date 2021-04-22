be13771bb9d0f424d7860ad464e4988502e8914b
Leandro Gustavo Neis <lgn200@gmail.com>
2021-04-21 22:33:59 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,25 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
-	private Department entity;
+	private Department department;
+	
+	private DepartmentService service;
 
 	@FXML
 	private TextField txtId;
@@ -30,18 +38,46 @@ public class DepartmentFormController implements Initializable {
 	@FXML
 	private Button btCancel;
 
-	public void setDepartment(Department entity) {
-		this.entity = entity;
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 
 	@FXML
-	public void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
+	public void onBtSaveAction(ActionEvent event) {
+		if(department == null ) {
+			new IllegalStateException("Entity was null");
+		}
+		if(service == null ) {
+			new IllegalStateException("Service was null");
+		}
+		
+		try {
+			department = getFormData();
+			service.saveOrUpdate(department);
+			Utils.currentStage(event).close();
+			
+		} catch (DbException e) {
+			Alerts.showAlert("Error saving object",null,e.getMessage(),AlertType.ERROR);	
+		}
+		
+	}
+
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
 	}
 
 	@FXML
-	public void onBtCancelAction() {
-		System.out.println("onBtCancelAction");
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 
 	@Override
@@ -55,11 +91,11 @@ public class DepartmentFormController implements Initializable {
 	}
 	
 	public void updateFormData() {
-		if(entity == null) {
+		if(department == null) {
 			throw new IllegalStateException("Entity was null");
 		}
-		txtId.setText(String.valueOf(entity.getId()));
-		txtName.setText(entity.getName());
+		txtId.setText(String.valueOf(department.getId()));
+		txtName.setText(department.getName());
 	}
 
 }
